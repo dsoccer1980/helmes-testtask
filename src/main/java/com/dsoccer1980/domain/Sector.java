b29a9e12de97fdab91b8c4646f08e6f936877114
dsoccer1980 <dsoccer1980@gmail.com>
2019-06-05 14:18:28 +0300

    Add Sector entity and repository

---
+++ src/main/java/com/dsoccer1980/domain/Sector.java
@@ -0,0 +1,21 @@
+package com.dsoccer1980.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Sector {
+
+    @Id
+    private int id;
+    private int parentId;
+    private String name;
+}
---
+++ src/main/java/com/dsoccer1980/repository/SectorRepository.java
@@ -0,0 +1,12 @@
+package com.dsoccer1980.repository;
+
+import com.dsoccer1980.domain.Sector;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+public interface SectorRepository extends JpaRepository<Sector, Integer> {
+
+}
